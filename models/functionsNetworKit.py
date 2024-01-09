afb551e7a105e5cc804646789c53e709222713c0
Bruno Deprez <64314973+B-Deprez@users.noreply.github.com>
2024-01-09 20:11:20 +0100

    Update functionsNetworKit.py

--- models/functionsNetworKit.py
+++ models/functionsNetworKit.py
@@ -1,1 +1,43 @@
-import networkit as nkimport pandas as pddef assign_att(u, att, val):    att[u] = valdef betweenness_nk(G_nk, path):    psp_list = []    G_nk.forNodes(lambda u: psp_list.append(u))        print("Calculating betweenness...")    betweenness_full = nk.centrality.Betweenness(G_nk)    betweenness_full.run()    print("Done")        betweenness_att = G_nk.attachNodeAttribute("betweenness_full", float)    G_nk.forNodes(lambda u: assign_att(u, betweenness_att, betweenness_full.score(u)))        betweenness_list = []    G_nk.forNodes(lambda u: betweenness_list.append(betweenness_att[u]))        betweenness_df = pd.DataFrame({"PSP":psp_list, "Betweenness":betweenness_list})        betweenness_df.to_csv(path)    def closeness_nk(G_nk, path):    psp_list = []    G_nk.forNodes(lambda u: psp_list.append(u))        print("Calculating closeness...")    closeness_full = nk.centrality.Closeness(G_nk, True, True)    closeness_full.run()    print("Done")        closeness_att = G_nk.attachNodeAttribute("closeness_full", float)    G_nk.forNodes(lambda u: assign_att(u, closeness_att, closeness_full.score(u)))        closeness_list = []    G_nk.forNodes(lambda u: closeness_list.append(closeness_att[u]))        closeness_df = pd.DataFrame({"PSP":psp_list, "Closeness":closeness_list})        closeness_df.to_csv(path)
+import networkit as nk
+import pandas as pd
+
+def assign_att(u, att, val):
+    att[u] = val
+
+def betweenness_nk(G_nk, path):
+    psp_list = []
+    G_nk.forNodes(lambda u: psp_list.append(u))
+    
+    print("Calculating betweenness...")
+    betweenness_full = nk.centrality.Betweenness(G_nk)
+    betweenness_full.run()
+    print("Done")
+    
+    betweenness_att = G_nk.attachNodeAttribute("betweenness_full", float)
+    G_nk.forNodes(lambda u: assign_att(u, betweenness_att, betweenness_full.score(u)))
+    
+    betweenness_list = []
+    G_nk.forNodes(lambda u: betweenness_list.append(betweenness_att[u]))
+    
+    betweenness_df = pd.DataFrame({"PSP":psp_list, "Betweenness":betweenness_list})
+    
+    betweenness_df.to_csv(path)
+    
+def closeness_nk(G_nk, path):
+    psp_list = []
+    G_nk.forNodes(lambda u: psp_list.append(u))
+    
+    print("Calculating closeness...")
+    closeness_full = nk.centrality.Closeness(G_nk, True, True)
+    closeness_full.run()
+    print("Done")
+    
+    closeness_att = G_nk.attachNodeAttribute("closeness_full", float)
+    G_nk.forNodes(lambda u: assign_att(u, closeness_att, closeness_full.score(u)))
+    
+    closeness_list = []
+    G_nk.forNodes(lambda u: closeness_list.append(closeness_att[u]))
+    
+    closeness_df = pd.DataFrame({"PSP":psp_list, "Closeness":closeness_list})
+    
+    closeness_df.to_csv(path)
