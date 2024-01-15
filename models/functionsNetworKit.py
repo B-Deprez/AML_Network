5bdd95136ad0f0fa05b846e750fe90b00d8cc273
B-Deprez <bruno.r.deprez@gmail.com>
2024-01-15 11:17:12 +0100

    add eigenvector centrality

--- models/functionsNetworKit.py
+++ models/functionsNetworKit.py
@@ -41,3 +41,22 @@ def closeness_nk(G_nk):
     closeness_df = pd.DataFrame({"PSP":psp_list, "Closeness":closeness_list})
     
     return(closeness_df)
+
+def eigenvector_nk(G_nk):
+    psp_list = []
+    G_nk.forNodes(lambda u: psp_list.append(u))
+    
+    print("Calculating eigenvector cent. ...")
+    eigen_full = nk.centrality.EigenvectorCentrality(G_nk)
+    eigen_full.run()
+    print("Done")
+    
+    eigen_att = G_nk.attachNodeAttribute("eigen_full", float)
+    G_nk.forNodes(lambda u: assign_att(u, eigen_att, eigen_full.score(u)))
+    
+    eigen_list = []
+    G_nk.forNodes(lambda u: eigen_list.append(eigen_att[u]))
+    
+    eigen_df = pd.DataFrame({"PSP":psp_list, "Eigenvector":eigen_list})
+    
+    return(eigen_df)
