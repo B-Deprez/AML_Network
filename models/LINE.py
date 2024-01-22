57418d01032d9b2007752b5f0e0af5fd2d7f458c
B-Deprez <bruno.r.deprez@gmail.com>
2024-01-22 17:40:15 +0100

    finish LINE for all weights 1

--- models/LINE.py
+++ models/LINE.py
@@ -6,11 +6,10 @@ from torch.nn import Embedding
 import torch.nn as nn
 from torch.utils.data import DataLoader
 
-from torch_geometric.typing import WITH_PYG_LIB, WITH_TORCH_CLUSTER
 from torch_geometric.utils import sort_edge_index
 from torch_geometric.utils.num_nodes import maybe_num_nodes
 
-class LINE(nn.Module):
+class LINE_w1(nn.Module):
     def __init__(
         self,
         edge_index: Tensor,
@@ -49,12 +48,12 @@ class LINE(nn.Module):
 
     @torch.jit.export
     def neg_sample(self, batch: Tensor) -> Tensor:
-        batch = batch.repeat(self.walks_per_node)
+        batch = batch.repeat(self.num_negative_samples)
 
         ns = torch.randint(self.num_nodes, (batch.size(0), 1),
                            dtype=batch.dtype, device=batch.device)
         ns = torch.cat([batch.view(-1, 1), ns], dim=-1) #concatenates the tensors
-        return(ns)
+        return ns
     
     @torch.jit.export
     def pos_sample(self, batch: Tensor) -> Tensor:
@@ -78,4 +77,38 @@ class LINE(nn.Module):
             batch = torch.tensor(batch)
         return self.pos_sample(batch), self.neg_sample(batch)
     
-    
+    @torch.jit.export
+    def loss(self, ps: Tensor, ns: Tensor) -> Tensor:
+        r"""Computes the loss given positive and negative random walks."""
+        if self.order == 1:
+            start, rest = ps[:, 0], ps[:, 1:].contiguous().view(-1)
+
+            h_start = self.embedding(start).view(ps.size(0), 1, self.embedding_dim)
+            h_rest = self.embedding(rest.view(-1)).view(ps.size(0), -1, self.embedding_dim)
+
+            out = (h_start * h_rest).sum(dim=-1).view(-1)
+            pos_loss = -torch.sigmoid(out).mean()
+            neg_loss = 0
+
+        else:
+            # Positive loss.
+            start, res = ps[:, 0], ps[:, 1:].contiguous().view(-1)
+            
+            h_start = self.embedding(start).view(ps.size(0), 1, self.embedding_dim)
+            h_rest = self.embedding(res.view(-1)).view(ps.size(0), -1, self.embedding_dim)
+            
+            out = (h_start * h_rest).sum(dim=-1).view(-1)
+            
+            pos_loss = -torch.log(torch.sigmoid(out) + self.EPS).mean()
+
+            # Negative loss.
+            start, res = ns[:, 0], ns[:, 1:].contiguous().view(-1)
+
+            h_start = self.embedding(start).view(ns.size(0), 1, self.embedding_dim)
+            h_rest = self.embedding(res.view(-1)).view(ns.size(0), -1, self.embedding_dim)
+            
+            out = (h_start * h_rest).sum(dim=-1).view(-1)
+            neg_loss = -torch.log(1 - torch.sigmoid(out) + self.EPS).mean()
+            
+
+        return pos_loss + neg_loss
