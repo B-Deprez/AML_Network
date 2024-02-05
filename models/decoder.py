f87a0a02068dba0b99574efa5ec431289c1bd434
B-Deprez <bruno.r.deprez@gmail.com>
2024-02-05 13:13:47 +0100

    add softmax as final layer

--- models/decoder.py
+++ models/decoder.py
@@ -5,7 +5,9 @@ class Decoder_linear(nn.Module):
     def __init__(self, embedding_dim, output_dim=2):
         super().__init__()
         self.layer1 = nn.Linear(embedding_dim, output_dim)
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, embedding):
         h = self.layer1(embedding)
+        h = self.softmax(h)
         return h
