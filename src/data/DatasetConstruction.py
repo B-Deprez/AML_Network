34c11c6c7a3956a7b2b2047954dcd3fb16d89c9f
B-Deprez <bruno.r.deprez@gmail.com>
2024-10-17 14:07:23 +0200

    add ibm data graph construction

--- src/data/DatasetConstruction.py
+++ src/data/DatasetConstruction.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import torch
-from torch_geometric.data import Data
 from utils.Network import network_AML
 
 #### Elliptic dataset ####
@@ -33,6 +32,75 @@ def load_elliptic():
     test_mask = (time_step >= 40) & (y != 2)
     
     ntw = network_AML(feat_df, edge_df, train_mask=train_mask, val_mask=val_mask, test_mask=test_mask)
+
+    return(ntw)
+
+#### IBM dataset ####
+from datetime import timedelta
+import os
+
+def preprocess_ibm():
+    date_format = '%Y/%m/%d %H:%M'
+
+    data_df = pd.read_csv('data/IBM/LI-Small_Trans.csv')
+    data_df['Timestamp'] = pd.to_datetime(data_df['Timestamp'], format=date_format)
+    data_df.sort_values('Timestamp', inplace=True)
+    data_df = data_df[data_df['Account']!= data_df['Account.1']]
+    data_df.reset_index(drop=True, inplace=True)
+    data_df.reset_index(inplace=True)
+
+    data_df_accounts = data_df[['index', 'Account', 'Account.1', 'Timestamp']]
+    delta = 12*60 # 12 hours
+
+    num_obs = len(data_df_accounts)
+    pieces = 100
+
+    source = []
+    target = []
+
+    for i in range(pieces):
+        start = i*num_obs//pieces
+        end = (i+1)*num_obs//pieces
+        data_df_right = data_df_accounts[start:end]
+        min_timestamp = data_df_right['Timestamp'].iloc[0]
+        max_timestamp = data_df_right['Timestamp'].iloc[-1]
+
+        data_df_left = data_df_accounts[(data_df_accounts['Timestamp']>=min_timestamp-timedelta(minutes=delta)) & (data_df_accounts['Timestamp']<=max_timestamp)]
+
+        data_df_join = data_df_left.merge(data_df_right, left_on='Account.1', right_on='Account', suffixes=('_1', '_2'))
+
+        for j in range(len(data_df_join)):
+            row = data_df_join.iloc[j]
+            delta_trans = row['Timestamp_2']-row['Timestamp_1']
+            if (delta_trans.days*24*60+delta_trans.seconds/60 <= delta) & (delta_trans.days*24*60+delta_trans.seconds/60 >= 0):
+                source.append(row['index_1'])
+                target.append(row['index_2'])
+
+    pd.DataFrame({'txId1': source, 'txId2': target}).to_csv('data/IBM/edges.csv', index=False)
+
+def load_ibm():
+    path = 'data/IBM'
+    if not os.path.exists(path+'/edges.csv'):
+        preprocess_ibm()
+    df_edges = pd.read_csv(path+'/edges.csv')
+
+    df_features = pd.read_csv(path+'/LI-Small_Trans.csv')
+    df_features.columns = ['txId', 'Timestamp', 'From Bank', 'Account', 'To Bank', 'Account.1', 'Amount Received', 'Receiving Currency', 'Amount Paid', 'Payment Currency', 'Payment Format', 'class']
+    df_features = df_features[['txId', 'Timestamp', 'Amount Received', 'Receiving Currency', 'Amount Paid', 'Payment Currency', 'Payment Format', 'class']]
+
+    # Timestamp based split:
+    mask = torch.tensor([False]*df_features.shape[0])
+    train_size = int(0.6*df_features.shape[0])
+    val_size = int(0.2*df_features.shape[0])
+
+    train_mask = mask.clone()
+    train_mask[:train_size] = True
+    val_mask = mask.clone()
+    val_mask[train_size:train_size+val_size] = True
+    test_mask = mask.clone()
+    test_mask[train_size+val_size:] = True
+
+    ntw = network_AML(df_features, df_edges, train_mask=train_mask, val_mask=val_mask, test_mask=test_mask)
 
     return(ntw)
 
